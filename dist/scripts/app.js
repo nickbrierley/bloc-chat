384cd62ca7fafeb6d1f6d12ff42ba94774db7586
nickbrierley <nickcbrierley@gmail.com>
2017-03-13 11:52:02 -0700

    adding rooms incomplete

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,6 +9,7 @@
         $stateProvider 
             .state('home', {
                 url: '/',
+                contoller: 'HomeCtrl as home',
                 templateUrl: '/templates/home.html'
         });
     }
