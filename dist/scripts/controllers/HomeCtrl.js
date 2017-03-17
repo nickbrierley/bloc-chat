fad5a519995bd50113479b5ed0bf4ed0f97e572c
nickbrierley <nickcbrierley@gmail.com>
2017-03-17 11:04:56 -0700

    removed unneeded function

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -2,10 +2,8 @@
     function HomeCtrl(Room) {
         this.rooms = Room.all;
         this.addRoom = function() {
-            this.submit = function () {
                 Room.add(this.newRoom);
             };
-        }
     }
        
     angular 
