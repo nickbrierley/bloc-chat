06b01ea097f715fa5548f350281e3ff314759e03
nickbrierley <nickcbrierley@gmail.com>
2017-03-16 20:09:50 -0700

    user story 2 created chat rooms

--- dist/scripts/controllers/HomeCtrl.js
+++ dist/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,13 @@
 (function() {
     function HomeCtrl(Room) {
         this.rooms = Room.all;
+        this.addRoom = function() {
+            this.submit = function () {
+                Room.add(this.newRoom);
+            };
+        }
     }
-    
+       
     angular 
         .module('blocChat')
         .controller('HomeCtrl', ['Room', HomeCtrl]);
